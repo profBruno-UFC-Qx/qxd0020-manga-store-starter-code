f3d9f02d5ab28d07eacba94007bcafcb0247175f
Bruno Mateus <brunomateus@gmail.com>
2024-09-16 08:30:03 -0300

    Página para criação de mangá

---
+++ frontend/src/pages/Admin/MangaForm.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="text-center">
+    <div class="spinner-border" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+  <template>
+    <div class="col-12 alert alert-dismissible fade show" role="alert">
+     
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    <img class="col-auto" />
+    <div class="row text-start">
+      <div class="col-12 mb-3">
+        <label for="coverInput" class="form-label">Manga cover</label>
+        <input type="file" id="coverInput" accept="image/*"  class="form-control">
+      </div>
+      <div class="col-12 mb-3">
+        <label for="titleInput" class="form-label">Manga title</label>
+        <input type="text" id="titleInput" class="form-control"  placeholder="an awesome title">
+      </div>
+      <div class="col-3 mb-3 ">
+        <label for="numberInput" class="form-label">Manga number</label>
+        <input type="number" id="numberInput" class="form-control" placeholder="volume number">
+      </div>
+      <div class="col-2 mb-3">
+        <label for="priceInput" class="form-label">Manga price</label>
+        <input type="text" id="priceInput" class="form-control"  placeholder="00.00">
+      </div>
+    </div>
+    <router-link to="/admin" class="btn btn-danger">Cancel</router-link>
+    <a class="btn btn-success" >Create</a>
+  </template>
+</template>
